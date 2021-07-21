b13cf737888b8ef2379b96303f58937e9ad44692
kimjunyun7 <kimjunyun7@gmail.com>
2021-07-21 15:27:33 +0700

    add SomeMethodsTest.java

---
+++ src/SomeMethodsTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SomeMethodsTest {
+
+	@Test
+	public void testWeight_index() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet_weight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGet_height() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
